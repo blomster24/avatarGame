21ca08958181e85a72de8a3b26fc1772bda43027
blomster24 <miguel_florez_@hotmail.com>
2022-11-20 10:09:04 -0500

    map added

--- js/avatar.js
+++ js/avatar.js
@@ -32,6 +32,7 @@ let inputAang
 let inputRoku
 let inputKyoshi
 let avatarPlayer
+let avatarPlayerObject
 let avatarAttack
 let buttonAir
 let buttonFire
@@ -48,30 +49,60 @@ let enemyVictories = 0
 let playerLives = 3
 let enemyLives = 3
 let canv = map.getContext('2d')
+let interval
+let mapBackground = new Image()
+mapBackground.src = "./assets/mapAvatar.jpg"
+let heightSearched
+let mapWidth = window.innerWidth - 20
+const widthMax = 600
+
+if (mapWidth > widthMax) {
+    mapWidth = widthMax - 20
+}
+
+heightSearched = mapWidth * 600 / 800
+map.width = mapWidth
+map.height = heightSearched
 
 // CLASSES
 
 class Avatar {
-    constructor(name, image, lives) {
+    constructor(name, image, lives, mapImage) {
         this.name = name;
         this.image = image;
         this.lives = lives;
         this.attacks = []
-        this.x = 20
-        this.y = 30
-        this.width = 80
-        this.height = 80
+        this.width = 50
+        this.height = 70
+        this.x = numRandom(0, map.width - this.width)
+        this.y = numRandom(0, map.height - this.height)
         this.mapImage = new Image()
-        this.mapImage.src = image
+        this.mapImage.src = mapImage
+        this.speedX = 0
+        this.speedY = 0
     }
+
+    drawAvatar() {
+        canv.drawImage(
+            this.mapImage,
+            this.x,
+            this.y,
+            this.width,
+            this.height)
+    }
+
 }
 
+let korra = new Avatar("Korra", "./assets/korra.png", 3, "./assets/korraCanvas.png")
+let aang = new Avatar("Aang", "./assets/Avatar_Aang.png", 5, "./assets/aangCanvas.png")
+let roku = new Avatar("Roku", "./assets/roku.png", 4, "./assets/rokuCanvas.png")
+let kyoshi = new Avatar("Kyoshi", "./assets/kyoshi.png", 3, "./assets/kyoshiCanvas.png")
 
-//let korra = new Avatar("Korra", "./assets/korra.png", 3)
-let korra = new Avatar("Korra", "./assets/korra.png", 3)
-let aang = new Avatar("Aang", "./assets/Avatar_Aang.png", 5)
-let roku = new Avatar("Roku", "./assets/roku.png", 4)
-let kyoshi = new Avatar("Kyoshi", "./assets/kyoshi.png", 3)
+//Enemies
+let korraEnemy = new Avatar("Korra", "./assets/korra.png", 3, "./assets/korraCanvas.png")
+let aangEnemy = new Avatar("Aang", "./assets/Avatar_Aang.png", 5, "./assets/aangCanvas.png")
+let rokuEnemy = new Avatar("Roku", "./assets/roku.png", 4, "./assets/rokuCanvas.png")
+let kyoshiEnemy = new Avatar("Kyoshi", "./assets/kyoshi.png", 3, "./assets/kyoshiCanvas.png")
 
 korra.attacks.push(
     { name: "💧", id: "water-button" },
@@ -87,6 +118,13 @@ aang.attacks.push(
     { name: "🌱", id: "earth-button" }
 )
 
+kyoshi.attacks.push(
+    { name: "💧", id: "water-button" },
+    { name: "💨", id: "air-button" },
+    { name: "🔥", id: "fire-button" },
+    { name: "🌱", id: "earth-button" }
+)
+
 roku.attacks.push(
     { name: "💧", id: "water-button" },
     { name: "💨", id: "air-button" },
@@ -94,12 +132,34 @@ roku.attacks.push(
     { name: "🌱", id: "earth-button" }
 )
 
-kyoshi.attacks.push(
+korraEnemy.attacks.push(
     { name: "💧", id: "water-button" },
     { name: "💨", id: "air-button" },
     { name: "🔥", id: "fire-button" },
     { name: "🌱", id: "earth-button" }
 )
+
+aangEnemy.attacks.push(
+    { name: "💧", id: "water-button" },
+    { name: "💨", id: "air-button" },
+    { name: "🔥", id: "fire-button" },
+    { name: "🌱", id: "earth-button" }
+)
+
+kyoshiEnemy.attacks.push(
+    { name: "💧", id: "water-button" },
+    { name: "💨", id: "air-button" },
+    { name: "🔥", id: "fire-button" },
+    { name: "🌱", id: "earth-button" }
+)
+
+rokuEnemy.attacks.push(
+    { name: "💧", id: "water-button" },
+    { name: "💨", id: "air-button" },
+    { name: "🔥", id: "fire-button" },
+    { name: "🌱", id: "earth-button" }
+)
+
 
 avatars.push(korra, aang, roku, kyoshi)
 
@@ -129,7 +189,10 @@ function startGame() {
 
     buttonRestart.addEventListener("click", restartGame)
 }
-
+//-----------------------------------------
+//-----------------------------------------
+//-----------------------------------------
+//-----------------------------------------
 function selectAvatarPlayer() {
     if (inputKorra.checked) {
         spanPlayerAvatar.innerHTML = inputKorra.id
@@ -149,11 +212,10 @@ function selectAvatarPlayer() {
 
     if (spanPlayerAvatar.innerHTML != "") {
         sectionSelectAvatar.style.display = "none"
-        drawAvatar()
-        //sectionSelectAttack.style.display = "flex"
-
         extractAttacks(avatarPlayer)
-        selectEnemysAvatar()
+        startMap()
+        //sectionSelectAttack.style.display = "flex"
+        //selectEnemysAvatar()
     }
 
 }
@@ -223,11 +285,12 @@ function attackSecuence() {
 }
 
 
-function selectEnemysAvatar() {
-    let random = numRandom(0, avatars.length - 1)
-
+function selectEnemysAvatar(enemy) {
+    /* let random = numRandom(0, avatars.length - 1)
     spanEnemysAvatar.innerHTML = avatars[random].name
-    enemyAvatarAttacks = avatars[random].attacks
+    enemyAvatarAttacks = avatars[random].attacks */
+    spanEnemysAvatar.innerHTML = enemy.name
+    enemyAvatarAttacks = enemy.attacks
     attackSecuence()
 }
 
@@ -251,7 +314,6 @@ function randomEnemyAttack() {
             enemyAttacks.push("EARTH")
             break
     }
-    console.log(enemyAttacks)
     startFight()
     //fight()
 }
@@ -360,21 +422,101 @@ function numRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-function drawAvatar() {
-    sectionSelectAttack.style.display = "none"
+function drawCanvas() {
+    avatarPlayerObject.x += avatarPlayerObject.speedX
+    avatarPlayerObject.y += avatarPlayerObject.speedY
     sectionViewMap.style.display = 'flex'
     canv.clearRect(0, 0, map.clientWidth, map.clientHeight)
-
-    canv.fillRect(5, 15, 20, 40) 
-
-    canv.drawImage(korra.image, korra.x, korra.y, korra.width, korra.height)
-
+    canv.drawImage(mapBackground, 0, 0, map.width, map.height)
+    //canv.drawImage(avatarPlayerObject.mapImage, avatarPlayerObject.x, avatarPlayerObject.y, avatarPlayerObject.width, avatarPlayerObject.height)
+    avatarPlayerObject.drawAvatar()
+    korraEnemy.drawAvatar()
+    aangEnemy.drawAvatar()
+    rokuEnemy.drawAvatar()
+    kyoshiEnemy.drawAvatar()
+    if (avatarPlayerObject.speedX !== 0 || avatarPlayerObject.speedY !== 0) {
+        checkCollision(korraEnemy)
+        checkCollision(aangEnemy)
+        checkCollision(rokuEnemy)
+        checkCollision(kyoshiEnemy)
+    }
 }
 
-function moveAvatar() {
-    korra.x += 5
-    drawAvatar()
+function moveUp() {
+    avatarPlayerObject.speedY = -5
+
+}
+function moveLeft() {
+    avatarPlayerObject.speedX -= 5
+}
+function moveRight() {
+    avatarPlayerObject.speedX = 5
+}
+function moveDown() {
+    avatarPlayerObject.speedY = 5
 }
 
+function stopMovement() {
+    avatarPlayerObject.speedX = 0
+    avatarPlayerObject.speedY = 0
+}
+
+function keyPressed(event) {
+    switch (event.key) {
+        case "ArrowUp":
+            moveUp()
+            break;
+        case "ArrowLeft":
+            moveLeft()
+            break;
+        case "ArrowRight":
+            moveRight()
+            break;
+        case "ArrowDown":
+            moveDown()
+            break;
+    }
+}
+
+function getObjectAvatar() {
+    for (let i = 0; i < avatars.length; i++) {
+        if (avatarPlayer == avatars[i].name) {
+            return avatars[i]
+        }
+    }
+}
+
+function startMap() {
+    avatarPlayerObject = getObjectAvatar()
+
+    interval = setInterval(drawCanvas, 50)
+    window.addEventListener("keydown", keyPressed)
+    window.addEventListener("keyup", stopMovement)
+}
+
+function checkCollision(enemy) {
+    const topAvatar = avatarPlayerObject.y
+    const bottomAvatar = avatarPlayerObject.y + avatarPlayerObject.height
+    const rightAvatar = avatarPlayerObject.x + avatarPlayerObject.width
+    const leftAvatar = avatarPlayerObject.x
+
+    const topEnemy = enemy.y
+    const bottomEnemy = enemy.y + enemy.height
+    const rightEnemy = enemy.x + enemy.width
+    const leftEnemy = enemy.x
+    if (
+        bottomAvatar < topEnemy ||
+        topAvatar > bottomEnemy ||
+        rightAvatar < leftEnemy ||
+        leftAvatar > rightEnemy
+    ) {
+        return
+    }
+    stopMovement()
+    clearInterval(interval)
+    sectionSelectAttack.style.display = "flex"
+    sectionViewMap.style.display = 'none'
+    selectEnemysAvatar(enemy)
+}
 
 window.addEventListener("load", startGame)
